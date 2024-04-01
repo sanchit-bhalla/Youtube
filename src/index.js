76c91405130edbf891b8dd43e837dfbbf516ac9b
sanchit-bhalla <sanchitbhalla91@gmail.com>
2024-04-01 13:33:56 +0530

    Add model, routes and controller templates

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ connectDB()
     });
 
     server = app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running at port: ${process.env.PORT || 8000}`);
+      console.log(`⚙️ Server is running at port: ${process.env.PORT || 8000}`);
     });
   })
   .catch((err) => console.log(`MONGO db connection failed !!! `, err));
